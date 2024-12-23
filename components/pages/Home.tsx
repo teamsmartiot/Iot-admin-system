5adbf8a974f1e8e69d26a9ef850d8361a53a8352
teamsmartiot <teamsmartiot2001@gmail.com>
2024-12-23 10:07:30 +0700

    xlcs

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -24,6 +24,7 @@ export const Home = () => {
 	return (
 		<div className='h-full bg-gradient-to-r from-white via-blue-400 to-white flex justify-center items-center flex-col gap-4 relative'>
 			<div className='font-bold text-4xl absolute top-5 text-center mt-7 text-black uppercase'>
+				<p>Trường THPT Hòn Đất </p>
 				<p> Cuộc thi KHKT cấp tỉnh học sinh trung học </p>
 				<p>năm học 2024-2025</p>
 			</div>
