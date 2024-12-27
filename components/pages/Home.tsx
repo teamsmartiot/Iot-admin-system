728b64c43e16dad786f2cb180a772b24aabfda8a
teamsmartiot <teamsmartiot2001@gmail.com>
2024-12-27 14:42:34 +0700

    fix

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -22,11 +22,11 @@ export const Home = () => {
 	};
 
 	return (
-		<div className='h-full bg-gradient-to-r from-white via-blue-400 to-white flex justify-center items-center flex-col gap-4 relative'>
-			<div className='font-bold text-4xl absolute top-5 text-center mt-7 text-white drop-shadow-lg uppercase'>
-				<p> Cuộc thi KHKT cấp tỉnh học sinh trung học năm học 2024-2025</p>
+		<div className='h-full bg-gradient-to-r from-blue-100 via-blue-400 to-blue-500 flex justify-center items-center flex-col gap-4 relative'>
+			<div className='font-bold  absolute top-5 text-center mt-7 text-white drop-shadow-lg uppercase'>
+				<p className='text-4xl'> Cuộc thi KHKT cấp tỉnh học sinh trung học năm học 2024-2025</p>
 
-				<p>Trường THPT Hòn Đất </p>
+				<p className='mt-4 text-3xl'>Trường THPT Hòn Đất </p>
 			</div>
 			<p className='font-medium text-white text-xl uppercase'>Trạng thái các ô tủ</p>
 			<div className='grid grid-cols-3 gap-6'>
